0d374177b84c46ad15c3a155085f571cf59d8fc5
Rachel L <rachelli@college.harvard.edu>
2020-07-15 01:07:01 -0400

    about page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -16,10 +16,11 @@ export default function Navbar() {
     return (
     <div>
         <header style={{ marginBottom: `6rem` }}>
-            <p className="title" style={{fontSize:'32px', fontWeight:'bold'}}>SOMETITLE</p>
+            <Link to="/" className="title" style={{fontSize:'32px', fontWeight:'bold', textDecoration:'none'
+            }}>TECH4BLM</Link>
             <ul className="navRight">
                 <ListLink to="about">ABOUT</ListLink>
-                <ListLink to="about">COLLABORATE</ListLink>
+                <ListLink to="getInvolved">GET INVOLVED</ListLink>
                 <ListLink to="contact">CONTACT</ListLink>
             </ul>
         </header>
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,21 @@
 import React from "react"
-import { Link } from "gatsby"
+import Navbar from "../components/navbar"
+import "../styles/styles.css"
+import blm from "../../public/static/images/blm.jpg"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+export default function About() {
+  return (
+    <div>
+      <Navbar/>
+      <div className="pageLayout">
+        <img src={blm}></img>
+        <h1>About</h1>
+        <p>Tech4BLM is a curated database of organizations working to create change at the intersection of social injustice and technology. 
+        In light of recent events, we have focused on highlighting social justice organizations that address racial injustice and inequality, 
+        advocate for policy reform in the prison and police systems, and work towards creating equal opportunity for marginalized peoples.</p>
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
-
-export default SecondPage
+        <p>If you know of other organizations that are performing crucial work in these areas, please fill out the form below to suggest additions to our database. Thank you!</p>
+      </div>
+    </div>
+  )
+} 
